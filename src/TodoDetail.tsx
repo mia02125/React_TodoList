ab1e53f9c077e3be6d0723ee8e86281a11ff263a
mia02125 <mia02125@naver.com>
2021-10-28 18:11:37 +0900

    jhbjh\\

--- src/TodoDetail.tsx
+++ src/TodoDetail.tsx
@@ -4,58 +4,62 @@ import { useTodo, todoStateData, ITodo } from './TodoData';
 import './App.css';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
-/**
- * Common : Input
- * @param props ITodo
- * @returns 
- */
- const Input = (props : { value? : string }) => {
-
-  return (
-    <Fragment>
-      <input value={props.value}          ></input>
-    </Fragment>
-  )
-}
-/**
- * Common : Select
- * @param props value
- * @returns 
- * 
- */
- const Select = (props : { value? : string}) => {
-
-  return (
-    <Fragment>
-      <select  value={props.value}         >
-        <option value="">선택</option>
-        <option value="대기">대기</option>
-        <option value="진행">진행</option>
-        <option value="완료">완료</option>
-      </select>
-    </Fragment>
-  )
-}
-
 export const TodoDetail = () => {
 
   const todoState = useRecoilValue(todoStateData); 
   const todoItem = todoState.todos.find(item => item.id === todoState.selectedId);
   const { updateTodo } = useTodo();
-
-  const [ inputTodoItem, setInputTodoItem ] = useState<ITodo>({
+  
+  const [ updData, setUpdData ] = useState<ITodo>({
     content : '',
-    status : '' 
+    status : ''
   })
+
+  /**
+   * Common : Input
+   * @param props ITodo
+   * @returns 
+   */
+  const Input = (props : { value? : string }) => {
+
+    const [ content, setContent ] = useState<string>('');
+
+    return (
+      <Fragment>
+        <input value={content || props.value} onChange={e => setContent(e.target.value)} ></input>
+      </Fragment>
+    )
+  }
+  /**
+   * Common : Select
+   * @param props value
+   * @returns 
+   * 
+   */
+  const Select = (props : { value? : string}) => {
+
+    return (
+      <Fragment>
+        <select  value={props.value}         >
+          <option value="">선택</option>
+          <option value="대기">대기</option>
+          <option value="진행">진행</option>
+          <option value="완료">완료</option>
+        </select>
+      </Fragment>
+    )
+  }
+
+
 
   return (
     <div className="main-item3">
       <span>
         ID : <span>{todoItem?.id}</span>
         <br/>
-        할 일 : <Input value={inputTodoItem.content || todoItem?.content} />
+        할 일 : <Input value={todoItem?.content} />
         <br/>
-        상 태 : <Select value={inputTodoItem.content || todoItem?.status} />
+        상 태 : <Select value={todoItem?.status} />
       </span>
       <br/>
       <button onClick={() => updateTodo(inputTodoItem)}>수정</button>
